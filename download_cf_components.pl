60b003b46909937015646ef9ce43d8c086f6d1f8
Gwindfan <bluce_jyy@hotmail.com>
2016-10-18 14:23:26 +0800

    remove hard code

--- download_cf_components.pl
+++ download_cf_components.pl
@@ -1,28 +1,23 @@
-## Name  : download_cf_components.pl
-## Due to: poor domestic internet.  
-## How to: Run it on a US host and scp downloaded files to local host. 
-## Description: Use Perl fork to download and scp the files parallelly.
-
-use POSIX ":sys_wait_h";
+﻿use POSIX ":sys_wait_h";
 use Net::SCP::Expect;
 use warnings;
 use strict;
 
+if ( $#ARGV < 0 ){
+	die "Usage: $0 to_host [to_dir] [username] [password]";
+}
 ## Destination host infomation
-my $dest_host = '10.6.1.61';
-my $username  = 'root';
-my $password  = 'password';
-my $dest_dir  = '/tmp/cf';
+my $dest_host =  $ARGV[0] if $#ARGV > -1 ;
+my $dest_dir  = ( exists $ARGV[1] and '' ne $ARGV[1] ) ? $ARGV[1] : '/tmp';
+my $username  = ( exists $ARGV[2] and '' ne $ARGV[2] ) ? $ARGV[2] : 'root';
+my $password  = ( exists $ARGV[3] and '' ne $ARGV[3] ) ? $ARGV[3] : 'password';
 my $scpe = Net::SCP::Expect->new( host => $dest_host,
                                   user => $username,
                                   password => $password,
                                   timeout => 20,
                                   auto_yes => 1); 
 my %uri = ( 
-    'vagrant_1.8.6_x86_64.deb' => 'https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_x86_64.deb',
-    'virtualbox-5.1_5.1.6-110634-Ubuntu-trusty_amd64.deb' => 'http://download.virtualbox.org/virtualbox/5.1.6/virtualbox-5.1_5.1.6-110634~Ubuntu~trusty_amd64.deb',
-    'virtualbox_trusty64.box' => 'https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20161005.0.0/providers/virtualbox.box',
-    'virtualbox_precise64_.box' => 'https://atlas.hashicorp.com/hashicorp/boxes/precise64/versions/1.1.0/providers/virtualbox.box',
+'virtualbox_bosh-lite.box' => 'https://atlas.hashicorp.com/cloudfoundry/boxes/bosh-lite/versions/9000.131.0/providers/virtualbox.box',
 );
 my @children_pids;
 
@@ -64,7 +59,7 @@ foreach my $key (keys %uri) {
         }
 }
 while(1){
-        sleep 1;
+    sleep 1;
 }
 __END__
 ## Ubuntu trusty
@@ -74,13 +69,16 @@ my %uri = (
 	'Oracle_VM_VirtualBox_Extension_Pack-5.1.6-110634.vbox-extpack' => 'http://download.virtualbox.org/virtualbox/5.1.6/Oracle_VM_VirtualBox_Extension_Pack-5.1.6-110634.vbox-extpack',
 	'virtualbox_trusty64.box' => 'https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20161005.0.0/providers/virtualbox.box',
 	'virtualbox_precise64_.box' => 'https://atlas.hashicorp.com/hashicorp/boxes/precise64/versions/1.1.0/providers/virtualbox.box',
-);
-
+	'virtualbox_precise32.box' => ' https://atlas.hashicorp.com/hashicorp/boxes/precise32/versions/1.0.0/providers/virtualbox.box',
+); 
 ## MAC OS
 my $uri = (
 	'vagrant_1.8.6.dmg' => 'https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6.dmg',
 	'VirtualBox-5.1.6-110634-OSX.dmg' => 'http://download.virtualbox.org/virtualbox/5.1.6/VirtualBox-5.1.6-110634-OSX.dmg',
 	'Oracle_VM_VirtualBox_Extension_Pack-5.1.6-110634.vbox-extpack' => 'http://download.virtualbox.org/virtualbox/5.1.6/Oracle_VM_VirtualBox_Extension_Pack-5.1.6-110634.vbox-extpack',
-	'virtualbox-5.1_5.1.6-110634-Ubuntu-trusty_amd64.deb' => 'http://download.virtualbox.org/virtualbox/5.1.6/virtualbox-5.1_5.1.6-110634~Ubuntu~trusty_amd64.deb',
 	'virtualbox_trusty64.box' => 'https://atlas.hashicorp.com/ubuntu/boxes/trusty64/versions/20161005.0.0/providers/virtualbox.box',
+	'virtualbox_precise64.box' => 'https://atlas.hashicorp.com/hashicorp/boxes/precise64/versions/1.1.0/providers/virtualbox.box',
+	'virtualbox_precise32.box' => ' https://atlas.hashicorp.com/hashicorp/boxes/precise32/versions/1.0.0/providers/virtualbox.box',
 );
+
+'virtualbox_bosh-lite.box' => 'https://atlas.hashicorp.com/cloudfoundry/boxes/bosh-lite/versions/9000.131.0/providers/virtualbox.box',
